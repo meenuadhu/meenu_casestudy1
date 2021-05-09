632b8dbab4c13e200730e4d0ad5780cf272e57e1
meenuadhu <meenuadhu@gmail.com>
2021-05-10 01:00:19 +0530

    first commit : First version

--- todo.js
+++ todo.js
@@ -1,75 +1,67 @@
-// api url
-// const api_url =
-// 	"https://jsonplaceholder.typicode.com/todos";
+
+
+    // api url
+const api_url =
+	"https://jsonplaceholder.typicode.com/todos";
 
 // Defining async function
-// async function getapi(url) {
+async function getapi(url) {
 	
-// 	// Storing response
-// 	const response = await fetch(url);
+	// Storing response
+	const response = await fetch(url);
 	
-// 	// Storing data in form of JSON
-// 	var data = await response.json();
-// 	console.log(data);
-// 	if (response) {
-// 		hideloader();
-// 	}
-// 	show(data);
-// }
-// // Calling that async function
-// getapi(api_url);
+	// Storing data in form of JSON
+	var data = await response.json();
+	console.log(data);
+	if (response) {
+		hideloader();
+	}
+	show(data);
+}
+// Calling that async function
+getapi(api_url);
 
-// // Function to hide the loader
-// function hideloader() {
-// 	document.getElementById('loading').style.display = 'none';
-// }
-// // Function to define innerHTML for HTML table
-// function show(data) {
-// 	let tab =
-// 		`<tr>
-// 		 <th>id</th>
-// 		<th>title</th>
-// 		<th><input type="checkbox" id="status" name="status" >status</th>
-// 	    </tr>`;
+// Function to hide the loader
+function hideloader() {
+	document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+	let tab =
+		`<tr>
+		 <th>id</th>
+		<th>title</th>
+		<th><input type="checkbox" id="status" name="status" >status</th>
+		
+	    </tr>`;
 	
-// 	// Loop to access all rows
-// 	for (let r of data) {
-// 		tab += `<tr>
-//     <td>${r.id} </td>
-// 	<td>${r.title}</td>
-// 	<td><input type="checkbox" id="status" name="status" value=${r.completed}"> </td>	
-// </tr>`;
-// 	}
-// 	document.getElementById("todo").innerHTML = tab;
-// }
-// document.getElementById('select').onclick = function() {
-//   var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-//   if(checkboxes.length==5){
-//     alert("mission success");
-//   }
- 
-// }
+	// Loop to access all rows
+	for (let r of data) {
+		tab += `<tr>
+    <td>${r.id} </td>
+	<td>${r.title}</td>
+	<td><input type="checkbox" onclick=countcheck() id=${r.id} ${checkval(r.completed)} class="tasktask" value=${r.title}>${r.title}	 </td>
+	
+</tr>`;
 
-const URL = "https://jsonplaceholder.typicode.com/todos";
-const main = document.getElementById("main");
-// main.innerHTML = "<p>Loading...";
-fetch(URL)
-  .then((response) => response.json())
-  .then((tasks) => main.innerHTML = getListOfTasks(tasks));
-const getListOfTasks = (tasks) => {
-  const names = tasks
-    .map((task) => `<li class="tbln"><input type="checkbox" onclick=countcheck() id=${task.id} ${chkchk(task.completed)} class="tasktask" value=${task.title}>${task.title}</li>`)
-    .join("\n");
-return `<ul>${names}</ul>`;
-};
-function chkchk(val){
-console.log(val)  ;
-if(val){return "checked"} else {return null ;}
+	}
+	// Setting innerHTML as tab variable
+document.getElementById("todo").innerHTML = tab;
+	console.log(tab);
+}
+	function checkval(stat){
+console.log(stat)  ;
+if(stat){
+	return "checked";
+}
+ else {
+	 return null ;
+	}
 }
  function countcheck(){
 
     function myDisplayer() {
-        alert("You have successfully completed 5 tasks")
+        alert("Congrats , You have successfully completed 5 Tasks")
     }
     
     let Count = new Promise(function(myResolve, myReject) {
@@ -77,7 +69,7 @@ if(val){return "checked"} else {return null ;}
         x=markedCheckbox.length;
         console.log(x);
    
-        if (x == 5) {
+        if (x == 95) {
         myResolve("OK");
         } 
     });
@@ -86,3 +78,12 @@ if(val){return "checked"} else {return null ;}
         function() {myDisplayer();}
     );
 }
+
+
+
+
+ 
+
+
+
+
