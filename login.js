b150713cc9be7f9796343bbc390133ec4ef79daf
meenuadhu <meenuadhu@gmail.com>
2021-05-11 10:48:04 +0530

    first commit : First version

--- login.js
+++ login.js
@@ -1,298 +1,298 @@
-var state = [];
+// var state = [];
 
-// function setDefaultState() {
-//   var id = generateID();
-//   var baseState = {};
-//   baseState[id] = {
-//     status: "new",
-//     id: id,
-//     title: "This site uses 🍪to keep track of your tasks"
-//   };
+// // function setDefaultState() {
+// //   var id = generateID();
+// //   var baseState = {};
+// //   baseState[id] = {
+// //     status: "new",
+// //     id: id,
+// //     title: "This site uses 🍪to keep track of your tasks"
+// //   };
+// //   syncState(baseState);
+// // }
+
+// function generateID() {
+//   var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+//   return randLetter + Date.now();
+// }
+
+// function pushToState(title, status, id) {
+//   var baseState = getState();
+//   baseState[id] = { id: id, title: title, status: status };
 //   syncState(baseState);
 // }
 
-function generateID() {
-  var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-  return randLetter + Date.now();
-}
+// function setToDone(id) {
+//   var baseState = getState();
+//   if (baseState[id].status === 'new') {
+//     baseState[id].status = 'done'
+//   } else {
+//     baseState[id].status =  'new';
+//   }
 
-function pushToState(title, status, id) {
-  var baseState = getState();
-  baseState[id] = { id: id, title: title, status: status };
-  syncState(baseState);
-}
+//   syncState(baseState);
+// }
 
-function setToDone(id) {
-  var baseState = getState();
-  if (baseState[id].status === 'new') {
-    baseState[id].status = 'done'
-  } else {
-    baseState[id].status =  'new';
-  }
+// function deleteTodo(id) {
+//   console.log(id)
+//   var baseState = getState();
+//   delete baseState[id]
+//   syncState(baseState)
+// }
 
-  syncState(baseState);
-}
+// function resetState() {
+//   localStorage.setItem("state", null);
+// }
 
-function deleteTodo(id) {
-  console.log(id)
-  var baseState = getState();
-  delete baseState[id]
-  syncState(baseState)
-}
+// function syncState(state) {
+//   localStorage.setItem("state", JSON.stringify(state));
+// }
 
-function resetState() {
-  localStorage.setItem("state", null);
-}
+// function getState() {
+//   return JSON.parse(localStorage.getItem("state"));
+// }
 
-function syncState(state) {
-  localStorage.setItem("state", JSON.stringify(state));
-}
+// function addItem(text, status, id, noUpdate) {
+//   var id = id ? id : generateID();
+//   var c = status === "done" ? "danger" : "";
+//   var item =
+//     '<li data-id="' +
+//     id +
+//     '" class="animated flipInX ' +
+//     c +
+//     '"><div class="checkbox"><span class="close"><i class="fa fa-times"></i></span><label><span class="checkbox-mask"></span><input type="checkbox" />' +
+//     text +
+//     "</label></div></li>";
 
-function getState() {
-  return JSON.parse(localStorage.getItem("state"));
-}
+//   var isError = $(".form-control").hasClass("hidden");
 
-function addItem(text, status, id, noUpdate) {
-  var id = id ? id : generateID();
-  var c = status === "done" ? "danger" : "";
-  var item =
-    '<li data-id="' +
-    id +
-    '" class="animated flipInX ' +
-    c +
-    '"><div class="checkbox"><span class="close"><i class="fa fa-times"></i></span><label><span class="checkbox-mask"></span><input type="checkbox" />' +
-    text +
-    "</label></div></li>";
+//   if (text === "") {
+//     $(".err")
+//       .removeClass("hidden")
+//       .addClass("animated bounceIn");
+//   } else {
+//     $(".err").addClass("hidden");
+//     $(".todo-list").append(item);
+//   }
 
-  var isError = $(".form-control").hasClass("hidden");
+//   $(".refresh").removeClass("hidden");
 
-  if (text === "") {
-    $(".err")
-      .removeClass("hidden")
-      .addClass("animated bounceIn");
-  } else {
-    $(".err").addClass("hidden");
-    $(".todo-list").append(item);
-  }
+//   $(".no-items").addClass("hidden");
 
-  $(".refresh").removeClass("hidden");
+//   $(".form-control")
+//     .val("")
+//     .attr("placeholder", "✍️ Add item...");
+//   setTimeout(function() {
+//     $(".todo-list li").removeClass("animated flipInX");
+//   }, 500);
 
-  $(".no-items").addClass("hidden");
+//   if (!noUpdate) {
+//     pushToState(text, "new", id);
+//   }
+// }
 
-  $(".form-control")
-    .val("")
-    .attr("placeholder", "✍️ Add item...");
-  setTimeout(function() {
-    $(".todo-list li").removeClass("animated flipInX");
-  }, 500);
+// function refresh() {
+//   $(".todo-list li").each(function(i) {
+//     $(this)
+//       .delay(70 * i)
+//       .queue(function() {
+//         $(this).addClass("animated bounceOutLeft");
+//         $(this).dequeue();
+//       });
+//   });
 
-  if (!noUpdate) {
-    pushToState(text, "new", id);
-  }
-}
+//   setTimeout(function() {
+//     $(".todo-list li").remove();
+//     $(".no-items").removeClass("hidden");
+//     $(".err").addClass("hidden");
+//   }, 800);
+// }
 
-function refresh() {
-  $(".todo-list li").each(function(i) {
-    $(this)
-      .delay(70 * i)
-      .queue(function() {
-        $(this).addClass("animated bounceOutLeft");
-        $(this).dequeue();
-      });
-  });
+// $(function() {
+//   var err = $(".err"),
+//     formControl = $(".form-control"),
+//     isError = formControl.hasClass("hidden");
 
-  setTimeout(function() {
-    $(".todo-list li").remove();
-    $(".no-items").removeClass("hidden");
-    $(".err").addClass("hidden");
-  }, 800);
-}
+//   if (!isError) {
+//     formControl.blur(function() {
+//       err.addClass("hidden");
+//     });
+//   }
 
-$(function() {
-  var err = $(".err"),
-    formControl = $(".form-control"),
-    isError = formControl.hasClass("hidden");
+//   $(".add-btn").on("click", function() {
+//     var itemVal = $(".form-control").val();
+//     addItem(itemVal);
+//     formControl.focus();
+//   });
 
-  if (!isError) {
-    formControl.blur(function() {
-      err.addClass("hidden");
-    });
-  }
+//   $(".refresh").on("click", refresh);
 
-  $(".add-btn").on("click", function() {
-    var itemVal = $(".form-control").val();
-    addItem(itemVal);
-    formControl.focus();
-  });
+//   $(".todo-list").on("click", 'input[type="checkbox"]', function() {
+//     var li = $(this)
+//       .parent()
+//       .parent()
+//       .parent();
+//     li.toggleClass("danger");
+//     li.toggleClass("animated flipInX");
 
-  $(".refresh").on("click", refresh);
+//     setToDone(li.data().id);
 
-  $(".todo-list").on("click", 'input[type="checkbox"]', function() {
-    var li = $(this)
-      .parent()
-      .parent()
-      .parent();
-    li.toggleClass("danger");
-    li.toggleClass("animated flipInX");
+//     setTimeout(function() {
+//       li.removeClass("animated flipInX");
+//     }, 500);
+//   });
 
-    setToDone(li.data().id);
+//   $(".todo-list").on("click", ".close", function() {
+//     var box = $(this)
+//       .parent()
+//       .parent();
 
-    setTimeout(function() {
-      li.removeClass("animated flipInX");
-    }, 500);
-  });
+//     if ($(".todo-list li").length == 1) {
+//       box.removeClass("animated flipInX").addClass("animated                bounceOutLeft");
+//       setTimeout(function() {
+//         box.remove();
+//         $(".no-items").removeClass("hidden");
+//         $(".refresh").addClass("hidden");
+//       }, 500);
+//     } else {
+//       box.removeClass("animated flipInX").addClass("animated bounceOutLeft");
+//       setTimeout(function() {
+//         box.remove();
+//       }, 500);
+//     }
 
-  $(".todo-list").on("click", ".close", function() {
-    var box = $(this)
-      .parent()
-      .parent();
+//     deleteTodo(box.data().id)
+//   });
 
-    if ($(".todo-list li").length == 1) {
-      box.removeClass("animated flipInX").addClass("animated                bounceOutLeft");
-      setTimeout(function() {
-        box.remove();
-        $(".no-items").removeClass("hidden");
-        $(".refresh").addClass("hidden");
-      }, 500);
-    } else {
-      box.removeClass("animated flipInX").addClass("animated bounceOutLeft");
-      setTimeout(function() {
-        box.remove();
-      }, 500);
-    }
+//   $(".form-control").keypress(function(e) {
+//     if (e.which == 13) {
+//       var itemVal = $(".form-control").val();
+//       addItem(itemVal);
+//     }
+//   });
+//   $(".todo-list").sortable();
+//   $(".todo-list").disableSelection();
+// });
 
-    deleteTodo(box.data().id)
-  });
-
-  $(".form-control").keypress(function(e) {
-    if (e.which == 13) {
-      var itemVal = $(".form-control").val();
-      addItem(itemVal);
-    }
-  });
-  $(".todo-list").sortable();
-  $(".todo-list").disableSelection();
-});
-
-var todayContainer = document.querySelector(".today");
+// var todayContainer = document.querySelector(".today");
 
 
-var d = new Date();
+// var d = new Date();
 
 
-var weekday = new Array(7);
-weekday[0] = "Sunday 🖖";
-weekday[1] = "Monday 💪😀";
-weekday[2] = "Tuesday 😜";
-weekday[3] = "Wednesday 😌☕️";
-weekday[4] = "Thursday 🤗";
-weekday[5] = "Friday 🍻";
-weekday[6] = "Saturday 😴";
+// var weekday = new Array(7);
+// weekday[0] = "Sunday 🖖";
+// weekday[1] = "Monday 💪😀";
+// weekday[2] = "Tuesday 😜";
+// weekday[3] = "Wednesday 😌☕️";
+// weekday[4] = "Thursday 🤗";
+// weekday[5] = "Friday 🍻";
+// weekday[6] = "Saturday 😴";
 
 
-var n = weekday[d.getDay()];
+// var n = weekday[d.getDay()];
 
 
-var randomWordArray = Array(
-  "Oh my, it's ",
-  "Whoop, it's ",
-  "Happy ",
-  "Seems it's ",
-  "Awesome, it's ",
-  "Have a nice ",
-  "Happy fabulous ",
-  "Enjoy your "
-);
+// var randomWordArray = Array(
+//   "Oh my, it's ",
+//   "Whoop, it's ",
+//   "Happy ",
+//   "Seems it's ",
+//   "Awesome, it's ",
+//   "Have a nice ",
+//   "Happy fabulous ",
+//   "Enjoy your "
+// );
 
-var randomWord =
-  randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
-
-
-todayContainer.innerHTML = randomWord + n;
-
-$(document).ready(function() {
-  var state = getState();
-
-  if (!state) {
-    setDefaultState();
-    state = getState();
-  }
-
-  Object.keys(state).forEach(function(todoKey) {
-    var todo = state[todoKey];
-    addItem(todo.title, todo.status, todo.id, true);
-  });
-
-  var mins, secs, update;
-
-  init();
-  function init() {
-    (mins = 25), (secs = 59);
-  }
+// var randomWord =
+//   randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
 
 
-  set();
-  function set() {
-    $(".mins").text(mins);
-  }
+// todayContainer.innerHTML = randomWord + n;
+
+// $(document).ready(function() {
+//   var state = getState();
+
+//   if (!state) {
+//     setDefaultState();
+//     state = getState();
+//   }
+
+//   Object.keys(state).forEach(function(todoKey) {
+//     var todo = state[todoKey];
+//     addItem(todo.title, todo.status, todo.id, true);
+//   });
+
+//   var mins, secs, update;
+
+//   init();
+//   function init() {
+//     (mins = 25), (secs = 59);
+//   }
 
 
-  $("#start").on("click", start_timer);
-  $("#reset").on("click", reset);
-  $("#inc").on("click", inc);
-  $("#dec").on("click", dec);
-
-  function start_timer() {
-
-    set();
-
-    $(".dis").attr("disabled", true);
-
-    $(".mins").text(--mins);
-    $(".separator").text(":");
-    update_timer();
-
-    update = setInterval(update_timer, 1000);
-  }
-
-  function update_timer() {
-    $(".secs").text(secs);
-    --secs;
-    if (mins == 0 && secs < 0) {
-      reset();
-    } else if (secs < 0 && mins > 0) {
-      secs = 59;
-      --mins;
-      $(".mins").text(mins);
-    }
-  }
+//   set();
+//   function set() {
+//     $(".mins").text(mins);
+//   }
 
 
-  function reset() {
-    clearInterval(update);
-    $(".secs").text("");
-    $(".separator").text("");
-    init();
-    $(".mins").text(mins);
-    $(".dis").attr("disabled", false);
-  }
+//   $("#start").on("click", start_timer);
+//   $("#reset").on("click", reset);
+//   $("#inc").on("click", inc);
+//   $("#dec").on("click", dec);
+
+//   function start_timer() {
+
+//     set();
+
+//     $(".dis").attr("disabled", true);
+
+//     $(".mins").text(--mins);
+//     $(".separator").text(":");
+//     update_timer();
+
+//     update = setInterval(update_timer, 1000);
+//   }
+
+//   function update_timer() {
+//     $(".secs").text(secs);
+//     --secs;
+//     if (mins == 0 && secs < 0) {
+//       reset();
+//     } else if (secs < 0 && mins > 0) {
+//       secs = 59;
+//       --mins;
+//       $(".mins").text(mins);
+//     }
+//   }
 
 
-  function inc() {
-    mins++;
-    $(".mins").text(mins);
-  }
+//   function reset() {
+//     clearInterval(update);
+//     $(".secs").text("");
+//     $(".separator").text("");
+//     init();
+//     $(".mins").text(mins);
+//     $(".dis").attr("disabled", false);
+//   }
 
 
-  function dec() {
-    if (mins > 1) {
-      mins--;
-      $(".mins").text(mins);
-    } else {
-      alert("This is the minimum limit.");
-    }
-  }
-}); 
+//   function inc() {
+//     mins++;
+//     $(".mins").text(mins);
+//   }
+
+
+//   function dec() {
+//     if (mins > 1) {
+//       mins--;
+//       $(".mins").text(mins);
+//     } else {
+//       alert("This is the minimum limit.");
+//     }
+//   }
+// }); 
 
 // /////////////////////////////////////////////////////////////////
 
